fcb33f513885c1c2c90521079222454fb4aa6002
thpvrs <63070104@kmitl.ac.th>
2022-11-22 16:58:58 +0700

    lab12

---
+++ src/test/java/com/sop/chapter10/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter10.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
